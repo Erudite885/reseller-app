b01a91caab083473216ca4f0b46733cb7f70d7e3
Erudite885 <erudite885@gmail.com>
2026-05-12 15:53:58 +0100

    feat: update asset handling by removing OTA update workflow and enabling custom assets in APK builds

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.blockList = [/assets\/custom\/.*/];
-
-module.exports = config;
