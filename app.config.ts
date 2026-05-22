44148098b71f8c2284764f603e63de2220d1ef42
Erudite885 <erudite885@gmail.com>
2026-05-22 15:25:20 +0100

    fix: change jsEngine from jsc to hermes in app configuration

--- app.config.ts
+++ app.config.ts
@@ -59,7 +59,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       resizeMode: "cover",
       backgroundColor: RESELLER_CONFIG.theme?.primary || "#379114",
     },
-    jsEngine: "jsc",
+    jsEngine: "hermes",
     package: RESELLER_CONFIG.config?.androidPackageName,
     edgeToEdgeEnabled: true
   },
